a1ef652cdf98a4d27d673d2cfe51398f09afc039
patrik william <patrikwilliam001@gmail.com>
2021-05-22 14:20:42 +0800

    new dir'

---
+++ template/footer.php
@@ -0,0 +1,12 @@
+    <!-- footer -->
+    <div class="container py-4">
+    <footer class="pt-3 mt-4 text-muted border-top">
+      &copy; 2021-<?= date('Y')?> | Patrik William
+    </footer>
+  </div>
+    <!-- akhir footer -->
+
+    <!-- javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <!-- My css -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
+    <title>Yo! Patrik william</title>
+  </head>
+  <body>
+      <!-- navbar -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary text-white fixed-top">
+        <div class="container">
+           <a class="navbar-brand mx-2" href="" target="_blank">Patrik_Williamm</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="?page=kelas">kelas</a>
+              </li>
+              <hr class="bg-dark mb-0 p-0">
+              <li class="nav-item">
+                <a class="nav-link active" href="?page=guru">Guru</a>
+              </li>
+              <hr class="bg-dark mb-0 p-0">
+              <li class="nav-item">
+                <a class="nav-link active" href="?page=mata_pelajaran">Mata Pelajaran</a>
+              </li>
+              <hr class="bg-dark mb-0 p-0">
+              <li class="nav-item mx-4">
+                <a class="nav-link active btn btn-danger rounder" href="logout.php">Keluar</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <!-- akhir navbar -->
+    
+  
