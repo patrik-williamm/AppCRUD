f8904564d370c8056d366d76f8e15143ea2ff98d
patrik william <patrikwilliam001@gmail.com>
2021-06-02 16:47:35 +0800

    modified content.php'

--- content.php
+++ content.php
@@ -1,15 +1,31 @@
 <?php
-$page = $_GET['page'];
+// $page = $_GET['page'];
 
-if ($page=='mata_pelajaran') {
-  include 'modul/mata_pelajaran/mataPelajaran.php';
-}
-elseif ($page=='kelas') {
-	include 'modul/kelas/Kelas.php';
-} 
-elseif ($page=='guru') {
-	include 'modul/Guru/guru.php';
-}
-else{   
-	include 'modul/home/home.php';  
+// if ($page=='mata_pelajaran') {
+//   include 'modul/mata_pelajaran/mataPelajaran.php';
+// }
+// elseif ($page=='kelas') {
+// 	include 'modul/kelas/Kelas.php';
+// } 
+// elseif ($page=='guru') {
+// 	include 'modul/Guru/guru.php';
+// }
+// else{   
+// 	include 'modul/home/home.php';  
+// }
+
+if (isset($_GET['page'])) {
+	$page = $_GET['page'];
+
+	if ($page == 'mata_pelajaran') {
+		require_once 'modul/mata_pelajaran/mataPelajaran.php';
+	}elseif ($page=='kelas') {
+		require_once 'modul/kelas/Kelas.php';	
+	}elseif ($page=='guru') {
+		require_once 'modul/Guru/guru.php';
+	}else {
+		require_once 'modul/home/home.php';  
+	}
+}else{
+	die("...");
 }
