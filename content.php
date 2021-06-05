cb8ce89072d3597aa5f8e4bb253dc7e6bc124f61
patrik william <patrikwilliam001@gmail.com>
2021-06-05 17:19:29 +0800

    modified content.php

--- content.php
+++ content.php
@@ -2,33 +2,34 @@
 if (isset($_GET['page'])) {
 	$page = $_GET['page'];
 
-	if ($page == 'mata_pelajaran')
-	{
+	if ($page == 'mata_pelajaran'){
 		require_once 'modul/mata_pelajaran/mataPelajaran.php';
-	}
-	elseif ($page=='kelas') 
-	{
+
+	} elseif ($page=='kelas') {
 		require_once 'modul/kelas/Kelas.php';	
-	}
-	elseif ($page=='guru') 
-	{
+
+	} elseif ($page=='guru') {
 		require_once 'modul/Guru/guru.php';
-	}
-	elseif ($page=='new_mp') 
-	{
+
+	} elseif ($page=='siswa') {
+		require_once 'modul/siswa/siswa.php';
+
+	} elseif ($page=='new_mp') {
 		require_once 'modul/mata_pelajaran/tambahData.php';
-	}elseif ($page == 'new_gr') 
-	{
+
+	}elseif ($page == 'new_gr') {
 		require_once 'modul/Guru/tambahDatagr.php';
-	}elseif ($page == 'new_kls') 
-	{
+
+	}elseif ($page == 'new_kls') {
 		require_once 'modul/kelas/tambahDatakls.php';
-	}
-	else 
-	{
+
+	} elseif ($page == 'new_sw') {
+		require_once 'modul/siswa/tambahDatasw.php';
+
+	} else {
 		require_once 'modul/home/home.php';
+
 	}
-}else
-{
+}else {
 	require_once 'modul/home/home.php';  
 }
