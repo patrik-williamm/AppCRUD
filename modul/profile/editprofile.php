33dad154c68fcc3eb4d78e422ad513394afa5e69
patrik william <patrikwilliam001@gmail.com>
2021-06-23 08:11:13 +0800

    menambahkan session login

--- modul/profile/editprofile.php
+++ modul/profile/editprofile.php
@@ -2,10 +2,9 @@
 $email = null;
 if (isset($_SESSION['email'])) {
   $email = $_SESSION['email'];
-
 }
-$myUser = view("SELECT * FROM users WHERE email='$email'")[0];
 
+$myUser = view("SELECT * FROM users WHERE email='$email'")[0];
 
 function updateProfile($data) {
   global $conn;
@@ -64,7 +63,7 @@ if (isset($_POST['batal'])) {
               <input type="hidden" class="form-control" id="password" name="password" value="<?= $myUser['password'] ?>">
             </div>
             <div class="form-floating mb-3 col-md-8 offset-md-2">
-              <input type="text" class="form-control" id="status" name="status" value="<?= $myUser['status'] ?>">
+/@              <input type="text" class="form-control" id="status" name="status" value="<?= $myUser['status'] ?>">
               <label for="status">Status User</label>
             </div>
             <div class="mb-3 col-md-8 offset-md-2">
--- templates/header.php
+++ templates/header.php
@@ -40,7 +40,7 @@
   </head>
   <body>
       <!-- navbar -->
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary text-white shadow-sm">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary text-white shadow-sm fixed-top ">
         <div class="container">
            <a class="navbar-brand mx-2" href="admin.php">Patrik_Williamm</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
